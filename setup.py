9c641a445798d3ac23e8565793c54431095a1b5a
funlyonions <funguanainc@gmail.com>
2020-01-04 02:21:35 -0600

    Removed requirements file

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import os
-from setuptools import setup
+import codecs
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -7,6 +14,37 @@ from setuptools import setup
 # string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+class UploadCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+        self.status("Building Source distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("sudo twine upload dist/*")
+        sys.exit()
+
+
 
 setup(
     name = "baranomi",
@@ -17,12 +55,15 @@ setup(
     license = "BSD",
     keywords = "example documentation tutorial",
     url = "http://packages.python.org/an_example_pypi_project",
-    packages=['baranomi'],
+    py_modules=['baranomi'],
+    packages=find_packages(),
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
     ],
     install_requires=[],
+    cmdclass={"upload": UploadCommand},
 )
